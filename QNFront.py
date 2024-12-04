02ea7faf93c87ef0c5090a0777c44f38ad42b541
jDuhamel <jduhamel@umich.edu>
2024-12-04 18:08:48 -0500

    push-10.1

--- QNFront.py
+++ QNFront.py
@@ -1,3 +1,9 @@
-'''
-Where a front end will one day exist, hopefully.
-'''
+from QNBackTopo import *
+
+# Create Universal simulator
+# Create and build a network
+# Produce visual of network
+# Retrieve output of network
+# Esnure that output is as desired
+
+
